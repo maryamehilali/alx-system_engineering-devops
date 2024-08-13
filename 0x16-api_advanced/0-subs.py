73c90b27b182d271f5adbddbdc14e07429f40866
maryame <elhilali.maryame2@gmail.com>
2024-08-13 20:52:53 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,13 +11,15 @@ import requests
 def number_of_subscribers(subreddit):
     """Function definition"""
     url = "https://www.reddit.com"
-    headers = {'Accept': 'application/json',
-               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-               AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 \
-               Safari/537.36'}
-    Api_req = requests.get('{}/r/{}/about.json'.format(url, subreddit),
-                           headers=headers, allow_redirects=False)
-    if Api_req.status_code == 200 and 'subscribers' in Api_req.json()['data']:
+    headers = {
+        'Accept': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+        }
+    Api_req = requests.get(
+        '{}/r/{}/about.json'.format(url, subreddit),
+        headers=headers, allow_redirects=False
+        )
+    if Api_req.status_code == 200:
         return Api_req.json()['data']['subscribers']
     else:
         return 0
